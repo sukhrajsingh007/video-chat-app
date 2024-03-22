4c82f6bcc27019f15b031d726d847a0aea0e0382
Sukhraj Singh <sukhraj.singh@grazitti.local>
2024-03-22 18:40:19 +0530

    fonts2

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-// app.js
+console.log('GothamNarrow-Book_1.woff');
+// // app.js
 
-const axios = require('axios');
+// const axios = require('axios');
 
-// Example text for analysis
-const text = 'I love using the Aylien Text Analysis API!';
+// // Example text for analysis
+// const text = 'I love using the Aylien Text Analysis API!';
 
-// Parameters for sentiment analysis
-const params = {
-    text: text,
-    mode: 'document',
-    language: 'en'
-};
+// // Parameters for sentiment analysis
+// const params = {
+//     text: text,
+//     mode: 'document',
+//     language: 'en'
+// };
 
-// Headers for the request
-const headers = {
-    'X-Application-Id': '62d99fc5',
-    'X-Application-Key': 'a63411be81ebe955084b3aaca19e9264',
-    'Content-Type': 'application/x-www-form-urlencoded'
-};
+// // Headers for the request
+// const headers = {
+//     'X-Application-Id': '62d99fc5',
+//     'X-Application-Key': 'a63411be81ebe955084b3aaca19e9264',
+//     'Content-Type': 'application/x-www-form-urlencoded'
+// };
 
-// Aylien Text Analysis API endpoint URL
-const apiUrl = 'https://api.aylien.com/api/v1/sentiment';
+// // Aylien Text Analysis API endpoint URL
+// const apiUrl = 'https://api.aylien.com/api/v1/sentiment';
 
-// Perform sentiment analysis with Axios
-axios.post(apiUrl, params, { headers })
-    .then(response => {
-        console.log('Sentiment Analysis:', response.data);
-    })
-    .catch(error => {
-        console.error('Error:', error.response.data);
-    });
+// // Perform sentiment analysis with Axios
+// axios.post(apiUrl, params, { headers })
+//     .then(response => {
+//         console.log('Sentiment Analysis:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error.response.data);
+//     });
